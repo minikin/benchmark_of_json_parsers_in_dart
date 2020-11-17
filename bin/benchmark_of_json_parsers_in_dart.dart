5b6fd4a3e120f7f26fa4cb44ea99ece092ecd03d
Sasha Prokhorenko <djminikin@gmail.com>
2020-11-17 20:49:23 +0100

    Initial commit

---
+++ bin/benchmark_of_json_parsers_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:benchmark_of_json_parsers_in_dart/benchmark_of_json_parsers_in_dart.dart' as benchmark_of_json_parsers_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${benchmark_of_json_parsers_in_dart.calculate()}!');
+}
---
+++ test/benchmark_of_json_parsers_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:benchmark_of_json_parsers_in_dart/benchmark_of_json_parsers_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
